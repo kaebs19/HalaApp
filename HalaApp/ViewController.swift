5ae1d3b3acd97b3a4f46a6366f2bf3a3a9c99bc7
Mohammed Saleh <kaebs19@gmail.com>
2025-06-06 02:39:34 +0300

    Initial Commit

---
+++ HalaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HalaApp
+//
+//  Created by Mohammed Saleh on 06/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
